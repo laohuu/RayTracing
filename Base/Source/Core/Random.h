4f3f02153ed46121c6fca067e9c3af24ba32dd4d
老胡 <14170811@qq.com>
2023-09-30 21:05:28 +0800

    import glm

--- Base/Source/Core/Random.h
+++ Base/Source/Core/Random.h
@@ -3,6 +3,8 @@
 
 #include <random>
 
+#include <glm/glm.hpp>
+
 namespace Base
 {
     class Random
@@ -16,11 +18,20 @@ namespace Base
         {
             return min + (s_Distribution(s_RandomEngine) % (max - min + 1));
         }
-        
+
         static float Float()
         {
             return (float)s_Distribution(s_RandomEngine) / (float)std::numeric_limits<uint32_t>::max();
         }
+
+        static glm::vec3 Vec3() { return {Float(), Float(), Float()}; }
+
+        static glm::vec3 Vec3(float min, float max)
+        {
+            return {Float() * (max - min) + min, Float() * (max - min) + min, Float() * (max - min) + min};
+        }
+
+        static glm::vec3 InUnitSphere() { return glm::normalize(Vec3(-1.0f, 1.0f)); }
 
     private:
         static std::mt19937                                             s_RandomEngine;
